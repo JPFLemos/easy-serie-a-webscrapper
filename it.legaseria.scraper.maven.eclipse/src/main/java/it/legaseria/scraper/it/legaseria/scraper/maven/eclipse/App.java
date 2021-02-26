e26caf5647dca4d1dd20728ea47be9f0e953fce8
Joao Paulo Lemos <lemos.jpf@gmail.com>
2021-02-26 18:08:31 +0100

    refactoring

--- it.legaseria.scraper.maven.eclipse/src/main/java/it/legaseria/scraper/it/legaseria/scraper/maven/eclipse/App.java
+++ it.legaseria.scraper.maven.eclipse/src/main/java/it/legaseria/scraper/it/legaseria/scraper/maven/eclipse/App.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 import com.itextpdf.text.DocumentException;
 
 public class App {
-	public static void main(String[] args) throws FileNotFoundException, DocumentException {
-		Scanner scan = new Scanner(System.in);
-		UserInterface ui = new UserInterface(scan);
-		
-		ui.start();
-	}
+    public static void main(String[] args) throws FileNotFoundException, DocumentException {
+        Scanner scan = new Scanner(System.in);
+        UserInterface ui = new UserInterface(scan);
+
+        ui.start();
+    }
 }
--- it.legaseria.scraper.maven.eclipse/src/main/java/it/legaseria/scraper/it/legaseria/scraper/maven/eclipse/Scrapper.java
+++ it.legaseria.scraper.maven.eclipse/src/main/java/it/legaseria/scraper/it/legaseria/scraper/maven/eclipse/Scrapper.java
@@ -3,128 +3,123 @@ package it.legaseria.scraper.it.legaseria.scraper.maven.eclipse;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.HashMap;
-
 public class Scrapper {
-	
-	private HashMap<Integer, ArrayList<MatchDay>> rounds;
 
-	public void giornate(int daQuesta, int finoQuesta) {
-		this.rounds = new HashMap<>();
-		
-		for (int page = daQuesta; page <= finoQuesta; page++) {
-			String url = "http://www.legaseriea.it/it/serie-a/calendario-e-risultati/2020-21/UNICO/UNI/"
-					+ String.valueOf(page);
+    private HashMap<Integer, ArrayList<MatchDay>> rounds;
 
-			try {
-				// Creating HashMap to store all match of this round;
-				 ArrayList<MatchDay> daysOfPlay = new ArrayList<>();
-				 rounds.put(page, daysOfPlay);
+    public void giornate(int daQuesta, int finoQuesta) {
+        this.rounds = new HashMap<>();
 
-				// Using Jsoup to connect to the website
-				Document doc = Jsoup.connect(url).get();
+        for (int page = daQuesta; page <= finoQuesta; page++) {
+            String url = "http://www.legaseriea.it/it/serie-a/calendario-e-risultati/2020-21/UNICO/UNI/" + String.valueOf(page);
 
-				
-				// Starting each round of matches
-				System.out.println("");
-				System.out.printf("############## GIORNATA " + page + " ##############");
-				System.out.println("");
-				
-				
+            try {
+                // Creating HashMap to store all match of this round;
+                ArrayList<MatchDay> daysOfPlay = new ArrayList<>();
+                rounds.put(page, daysOfPlay);
 
-				// Fetching round info and all matches
-				Elements repos = doc.getElementsByClass("box-partita");
+                // Using Jsoup to connect to the website
+                Document doc = Jsoup.connect(url)
+                    .get();
 
-				// Call the method that extract and treats the data of each match
-				extractData(repos, daysOfPlay, page);
+                // Starting each round of matches
+                System.out.println("");
+                System.out.printf("############## GIORNATA " + page + " ##############");
+                System.out.println("");
 
-				// In case of any errors we throw an exception;
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+                // Fetching round info and all matches
+                Elements repos = doc.getElementsByClass("box-partita");
 
-	private void extractData(Elements repositories, ArrayList<MatchDay> daysOfPlay, int round) {
+                // Call the method that extract and treats the data of each match
+                extractData(repos, daysOfPlay, page);
 
-		for (Element repo : repositories) {
-			String matchDetails = repo.getElementsByClass("datipartita").text();
-			
+                // In case of any errors we throw an exception;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-			
-			//Match date and time
-			String[] dateAndTime = matchDetails.substring(0, 16).replaceAll("[a-zA-Z]", "").trim().split(" ");
-			String time = "Informazione non disponibile"; // Setting the default time to not available (in case it is available it will be updated accordingly)
+    private void extractData(Elements repositories, ArrayList<MatchDay> daysOfPlay, int round) {
 
-			if (dateAndTime.length > 1) {
-				time = dateAndTime[1];
-			}
+        for (Element repo : repositories) {
+            String matchDetails = repo.getElementsByClass("datipartita")
+                .text();
 
-			String displayDate = "";
-			if (dateAndTime[0].length() > 9) {
-				LocalDate dateAsDate = LocalDate.parse(dateAndTime[0], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-				displayDate = dateAsDate.format(DateTimeFormatter.ofPattern("EEEE, dd/MM/yyyy", Locale.ITALIAN));
-			}
-			
+            // Match date and time
+            String[] dateAndTime = matchDetails.substring(0, 16)
+                .replaceAll("[a-zA-Z]", "")
+                .trim()
+                .split(" ");
+            String time = "Informazione non disponibile"; // Setting the default time to not available (in case it is available it will be updated accordingly)
 
-			// Match Teams
-			String teams = repo.getElementsByClass("nomeSquadra").text();
-			String[] teamsArray = teams.split(" "); 
-			String teamOne = teamsArray[0];
-			String teamTwo = teamsArray[1];
-			if (teamOne.equals("Hellas")) {                               // When name is Hellas Verona the array is different
-				teamOne = teamsArray[0];
-				teamTwo = teamsArray[2];
-			}
+            if (dateAndTime.length > 1) {
+                time = dateAndTime[1];
+            }
 
-			// Match Channel
-			String channel = "Informazione non disponibile";
-			if (matchDetails.contains("Diretta")) {
-				channel = matchDetails.substring(matchDetails.lastIndexOf(":") + 2);
-			}
+            String displayDate = "";
+            if (dateAndTime[0].length() > 9) {
+                LocalDate dateAsDate = LocalDate.parse(dateAndTime[0], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                displayDate = dateAsDate.format(DateTimeFormatter.ofPattern("EEEE, dd/MM/yyyy", Locale.ITALIAN));
+            }
 
-			if (matchDetails.contains("Vedi")) {                             // Some matches weren't shown on TV and the scrapped info is different
-				channel = channel.substring(0, 4).trim();
-			}
+            // Match Teams
+            String teams = repo.getElementsByClass("nomeSquadra")
+                .text();
+            String[] teamsArray = teams.split(" ");
+            String teamOne = teamsArray[0];
+            String teamTwo = teamsArray[1];
+            if (teamOne.equals("Hellas")) { // When name is Hellas Verona the array is different
+                teamOne = teamsArray[0];
+                teamTwo = teamsArray[2];
+            }
 
-			
-			Match match = new Match(teamOne, teamTwo, round, displayDate, channel, time);
-			MatchDay matchDay = new MatchDay(displayDate);
-			if (!daysOfPlay.contains(matchDay)) {
-				daysOfPlay.add(matchDay);
-			}
-			
-			daysOfPlay.get(daysOfPlay.size() - 1).addMatch(match);
-			
-		}
-		
-		
-		
-		for (MatchDay matchday : daysOfPlay) {
-			
-			System.out.println("XXXXXXX");
-			System.out.println(matchday.getDate());
-			System.out.println("XXXXXXX");
-			
-			for (Match match : matchday.getMatches()) {
-				
-				match.print();
-			}
-			
-		}
-	}
-	
-	public HashMap<Integer, ArrayList<MatchDay>> getRounds() {
-		return this.rounds;
-	}
+            // Match Channel
+            String channel = "Informazione non disponibile";
+            if (matchDetails.contains("Diretta")) {
+                channel = matchDetails.substring(matchDetails.lastIndexOf(":") + 2);
+            }
+
+            if (matchDetails.contains("Vedi")) { // Some matches weren't shown on TV and the scrapped info is different
+                channel = channel.substring(0, 4)
+                    .trim();
+            }
+
+            Match match = new Match(teamOne, teamTwo, round, displayDate, channel, time);
+            MatchDay matchDay = new MatchDay(displayDate);
+            if (!daysOfPlay.contains(matchDay)) {
+                daysOfPlay.add(matchDay);
+            }
+
+            daysOfPlay.get(daysOfPlay.size() - 1)
+                .addMatch(match);
+
+        }
+
+        for (MatchDay matchday : daysOfPlay) {
+
+            System.out.println("XXXXXXX");
+            System.out.println(matchday.getDate());
+            System.out.println("XXXXXXX");
+
+            for (Match match : matchday.getMatches()) {
+
+                match.print();
+            }
+
+        }
+    }
+
+    public HashMap<Integer, ArrayList<MatchDay>> getRounds() {
+        return this.rounds;
+    }
 }
